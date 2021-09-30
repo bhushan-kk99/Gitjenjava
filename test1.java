28bf9ea719f73aca3faf66d35c64b32f2650c53e
bhushan-kk99 <khutarkar99@gmail.com>
2021-09-30 20:46:49 +0530

    Modified

--- test1.java
+++ test1.java
@@ -1,6 +1,6 @@
 class test1{
 		public static void main (String args [])
 		{
-		System.out.println("Welcome to Jenkins ");
+		System.out.println("Welcome to Git and Jenkins Integration  ");
 		}
 	   }
